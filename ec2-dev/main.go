2d3a460c8c4430ed933cf9da08d798f0fcaf0a3e
yhidetoshi <hyajima.1014@gmail.com>
2018-07-08 13:12:20 +0900

    複数インスタンスでも取得できるように修正

---
+++ ec2-dev/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+var (
+	svc = ec2.New(session.New())
+)
+
+func main() {
+	lambda.Start(Handler)
+}
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func Handler(ctx context.Context) {
+	result := getInstanceInfo()
+  for v, _ := range(result){
+			fmt.Println(result[v])
+	}
+
+}
+
+func getInstanceInfo() []string {
+	var tagName string
+	var instances []string
+
+	params := &ec2.DescribeInstancesInput{}
+	res, err := svc.DescribeInstances(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	for _, resInfo := range res.Reservations {
+		for _, instanceInfo := range resInfo.Instances {
+			for _, tagInfo := range instanceInfo.Tags {
+				if *tagInfo.Key == "Name" {
+					tagName = *tagInfo.Value
+				}
+			}
+			instances = append(instances, tagName)
+		}
+	}
+	return instances
+}
